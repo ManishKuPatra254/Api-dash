f4836a7cab1216c1a740cd97f3a55e93adce7696
Manish <manish.patra@celetel.com>
2025-03-07 01:13:32 +0530

    added the new ui in logina and all

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,81 +1,141 @@
 "use client";
 
-import { Button } from "./Button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuSubMenu,
+  DropdownMenuSubMenuContent,
+  DropdownMenuSubMenuTrigger,
+  DropdownMenuTrigger,
+} from "./DropdownMenu";
 import { cx, focusRing } from "../lib/utils";
-import { ChevronsUpDown, User } from "lucide-react";
+import {
+  RiArrowRightUpLine,
+  RiComputerLine,
+  RiMoonLine,
+  RiSunLine,
+} from "@remixicon/react";
+// import { useTheme } from "next-themes";
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { DropdownUserProfile } from "./DropdownUserProfile";
+function DropdownUserProfile() {
+  const [mounted, setMounted] = React.useState(false);
+  // const { theme, setTheme } = useTheme();
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
 
-interface UserProfileDesktopProps {
-  isCollapsed?: boolean;
-}
-
-export const UserProfileDesktop = ({
-  isCollapsed,
-}: UserProfileDesktopProps) => {
+  if (!mounted) {
+    return null;
+  }
   return (
-    <DropdownUserProfile>
-      <Button
-        aria-label="User settings"
-        variant="ghost"
-        className={cx(
-          isCollapsed ? "justify-center" : "justify-between",
-          focusRing,
-          "group flex w-full items-center rounded-md px-1 py-2 text-sm font-medium text-gray-900 hover:bg-gray-200/50 data-[state=open]:bg-gray-200/50 hover:dark:bg-gray-800/50 data-[state=open]:dark:bg-gray-900"
-        )}
-      >
-        {isCollapsed ? (
-          // h-8 to avoid layout shift with icon shown in isCollapsibled == false
-          <div className="flex h-8 items-center">
-            <User
-              className="size-5 shrink-0 text-gray-500 group-hover:text-gray-700 dark:text-gray-500 group-hover:dark:text-gray-300"
-              aria-hidden="true"
-            />
-          </div>
-        ) : (
-          <span className="flex items-center gap-3">
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button
+            aria-label="open settings"
+            className={cx(
+              focusRing,
+              "group rounded-full p-1 hover:bg-gray-100 data-[state=open]:bg-gray-100 hover:dark:bg-gray-400/10 data-[state=open]:dark:bg-gray-400/10"
+            )}
+          >
             <span
-              className={cx(
-                isCollapsed ? "size-5" : "size-8",
-                "flex shrink-0 items-center justify-center rounded-full border border-gray-300 bg-white text-xs text-gray-700 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300"
-              )}
+              className="flex size-8 shrink-0 items-center justify-center rounded-full border border-gray-300 bg-white text-xs font-medium text-gray-700 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300"
               aria-hidden="true"
             >
-              MK
+              ES
             </span>
-            <span className={cx(isCollapsed ? "hidden" : "block")}>
-              Pawan Kumar
-            </span>
-          </span>
-        )}
-        {!isCollapsed && (
-          <ChevronsUpDown
-            className="size-4 shrink-0 text-gray-500 group-hover:text-gray-700 group-hover:dark:text-gray-400"
-            aria-hidden="true"
-          />
-        )}
-      </Button>
-    </DropdownUserProfile>
-  );
-};
-
-export const UserProfileMobile = () => {
-  return (
-    <DropdownUserProfile align="end">
-      <Button
-        aria-label="User settings"
-        variant="ghost"
-        className={cx(
-          "group flex items-center rounded-md p-0.5 sm:p-1 text-sm font-medium text-gray-900 hover:bg-gray-200/50 data-[state=open]:bg-gray-200/50 hover:dark:bg-gray-800/50 data-[state=open]:dark:bg-gray-800/50"
-        )}
-      >
-        <span
-          className="flex size-8 sm:size-7 shrink-0 items-center justify-center rounded-full border border-gray-300 bg-white text-xs text-gray-700 dark:border-gray-800 dark:bg-gray-950 dark:text-gray-300"
-          aria-hidden="true"
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          align="end"
+          className="!min-w-[calc(var(--radix-dropdown-menu-trigger-width))]"
         >
-          ES
-        </span>
-      </Button>
-    </DropdownUserProfile>
+          <DropdownMenuLabel>emma.stone@acme.com</DropdownMenuLabel>
+          <DropdownMenuGroup>
+            <DropdownMenuSubMenu>
+              <DropdownMenuSubMenuTrigger>Theme</DropdownMenuSubMenuTrigger>
+              <DropdownMenuSubMenuContent>
+                <DropdownMenuRadioGroup
+                // value={theme}
+                // onValueChange={(value) => {
+                //   setTheme(value);
+                // }}
+                >
+                  <DropdownMenuRadioItem
+                    aria-label="Switch to Light Mode"
+                    value="light"
+                  >
+                    <RiSunLine className="size-4 shrink-0" aria-hidden="true" />
+                    Light
+                  </DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem
+                    aria-label="Switch to Dark Mode"
+                    value="dark"
+                  >
+                    <RiMoonLine
+                      className="size-4 shrink-0"
+                      aria-hidden="true"
+                    />
+                    Dark
+                  </DropdownMenuRadioItem>
+                  <DropdownMenuRadioItem
+                    aria-label="Switch to System Mode"
+                    value="system"
+                  >
+                    <RiComputerLine
+                      className="size-4 shrink-0"
+                      aria-hidden="true"
+                    />
+                    System
+                  </DropdownMenuRadioItem>
+                </DropdownMenuRadioGroup>
+              </DropdownMenuSubMenuContent>
+            </DropdownMenuSubMenu>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              Changelog
+              <RiArrowRightUpLine
+                className="mb-1 ml-1 size-3 shrink-0 text-gray-500"
+                aria-hidden="true"
+              />
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Documentation
+              <RiArrowRightUpLine
+                className="mb-1 ml-1 size-3 shrink-0 text-gray-500"
+                aria-hidden="true"
+              />
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              Join Slack community
+              <RiArrowRightUpLine
+                className="mb-1 ml-1 size-3 shrink-0 text-gray-500"
+                aria-hidden="true"
+              />
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem>
+              <Link to="/" className="w-full">
+                Sign out
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
-};
+}
+
+export { DropdownUserProfile };
